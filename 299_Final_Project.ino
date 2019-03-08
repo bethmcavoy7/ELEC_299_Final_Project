0ab02428cb2ef2f4e046eb98f905e42dec5af2d9
Nick T <17ndt@queensu.ca>
2019-03-07 22:21:18 -0500

    Update 299_Final_Project.ino

--- 299_Final_Project.ino
+++ 299_Final_Project.ino
@@ -1,8 +1,32 @@
 //ELEC 299 Final Project Teams 16, 17, 18
 
 
+#define bump1 2
+#define bump2 3
+#define M2 4
+#define E2 5
+#define E1 6
+#define M1 7
+#define grip 9
+#define tilt 10
+#define pan 11
+#define LEL 12
+#define LER 13
+
+#define IR A0
+#define grip A1
+#define frontIR A2
+#define leftIR A3
+#define centreIR A4
+#define rightIR A5
+
+//Need to include ASCII Character Definitions
+
+
 void setup() {
   // put your setup code here, to run once:
+  
+
 
 }
 
@@ -10,3 +34,5 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+
